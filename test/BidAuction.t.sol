19677de1de7ca05237b1dd947be79c5ae25646cf
SHAKOTN <shakotn@protonmail.com>
2023-10-04 16:07:17 +0300

    test: adding more tests and fuzzing

--- test/BidAuction.t.sol
+++ test/BidAuction.t.sol
@@ -54,8 +54,10 @@ contract TestBidAuction is Test {
         vm.stopPrank();
     }
 
-    function testRunAuctionHappy() public {
-        BidAuction auction = _createAuction(1000e6, 1e18);
+    function testRunAuctionHappy(uint256 _minBid, uint96 _auctionAmount) public {
+        _minBid = bound(_minBid, 1e18, 1000e18);
+        _auctionAmount = uint96(bound(_auctionAmount, 1e6, 100_000_000e6));
+        BidAuction auction = _createAuction(_auctionAmount, _minBid);
 
         // Give USDC to basedAdmin
         setStorage(basedAdmin, USDC.balanceOf.selector, address(USDC), type(uint96).max);
@@ -70,31 +72,94 @@ contract TestBidAuction is Test {
 
         // Now start bidding
         vm.prank(alice);
-        auction.bid{ value: 2e18 }();
-
+        auction.bid{ value: _minBid * 2 }();
+        uint256 aliceEthBalanceSnapshot = alice.balance;
         // Make sure highest bid and bidder are set
-        assertEq(auction.highestBid(), 2e18);
+        assertEq(auction.highestBid(), _minBid * 2);
         assertEq(auction.highestBidder(), alice);
 
         // Make sure alice's bid is set
-        assertEq(auction.bids(alice), 2e18);
+        assertEq(auction.bids(alice), _minBid * 2);
 
         // Bob wants to bid
         vm.prank(bob);
-        auction.bid{ value: 3e18 }();
+        auction.bid{ value: _minBid * 3 }();
 
         // Make sure highest bid and bidder are set
-        assertEq(auction.highestBid(), 3e18);
+        assertEq(auction.highestBid(), _minBid * 3);
 
         // Make sure bob's bid is set
-        assertEq(auction.bids(bob), 3e18);
-
+        assertEq(auction.bids(bob), _minBid * 3);
+        vm.prank(bob);
+        // As Bob is the highest bidder, she can't withdraw
+        vm.expectRevert("highest bidder can't withdraw");
+        auction.withdraw();
         // End auction and alice gets USDC
         vm.warp(block.timestamp + 7 days + 1);
         auction.end();
 
         // Make sure USDC is transferred to bob
         assertEq(USDC.balanceOf(alice), 0);
-        assertEq(USDC.balanceOf(bob), 1000e6);
+        assertEq(USDC.balanceOf(bob), _auctionAmount);
+
+        // Alice can withdraw now
+        vm.prank(alice);
+        auction.withdraw();
+        // Make sure alice gets her ETH back
+        assertEq(alice.balance, aliceEthBalanceSnapshot + _minBid * 2);
+    }
+
+    function testRunAuctionNoBidders(uint256 _minBid, uint96 _auctionAmount) public {
+        _minBid = bound(_minBid, 1e18, 1000e18);
+        _auctionAmount = uint96(bound(_auctionAmount, 1e6, 100_000_000e6));
+        BidAuction auction = _createAuction(_auctionAmount, _minBid);
+
+        // Give USDC to basedAdmin
+        setStorage(basedAdmin, USDC.balanceOf.selector, address(USDC), type(uint96).max);
+        uint256 balanceSnapshot = USDC.balanceOf(basedAdmin);
+        vm.startPrank(basedAdmin);
+        // Approve auction to spend USDC
+        USDC.approve(address(auction), type(uint96).max);
+        // Start auction
+        auction.start();
+        assertEq(USDC.balanceOf(basedAdmin), balanceSnapshot - _auctionAmount);
+        assertEq(auction.started(), true);
+        vm.stopPrank();
+
+        // End auction and owner gets USDC back, since there were no bidders
+        vm.warp(block.timestamp + 7 days + 1);
+        auction.end();
+        // Make sure owner get USDC back
+        assertEq(USDC.balanceOf(basedAdmin), balanceSnapshot);
+    }
+
+    function testCantWithdrawHighestBid(uint256 _minBid, uint96 _auctionAmount) public {
+        _minBid = bound(_minBid, 1e18, 1000e18);
+        _auctionAmount = uint96(bound(_auctionAmount, 1e6, 100_000_000e6));
+        BidAuction auction = _createAuction(_auctionAmount, _minBid);
+
+        // Give USDC to basedAdmin
+        setStorage(basedAdmin, USDC.balanceOf.selector, address(USDC), type(uint96).max);
+
+        vm.startPrank(basedAdmin);
+        // Approve auction to spend USDC
+        USDC.approve(address(auction), type(uint96).max);
+        // Start auction
+        auction.start();
+        assertEq(auction.started(), true);
+        vm.stopPrank();
+
+        // Now start bidding
+        vm.prank(alice);
+        auction.bid{ value: _minBid * 2 }();
+
+        // Make sure highest bid and bidder are set
+        assertEq(auction.highestBid(), _minBid * 2);
+        assertEq(auction.highestBidder(), alice);
+
+        vm.prank(alice);
+        // As Alice is the highest bidder, she can't withdraw
+        vm.expectRevert("highest bidder can't withdraw");
+        auction.withdraw();
     }
 }
